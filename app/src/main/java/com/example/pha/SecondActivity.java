bdc589aea55cb194977046612b19699acda31ee1
rocktim <rocktimsharma82@gmail.com>
2023-03-26 15:17:25 +0530

    Fragment added

--- app/src/main/java/com/example/pha/SecondActivity.java
+++ app/src/main/java/com/example/pha/SecondActivity.java
@@ -2,13 +2,31 @@ package com.example.pha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SecondActivity extends AppCompatActivity {
+import com.example.pha.Fragments.Fragment1;
+
+public class
+SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Fragment1()).commit();
+
+        //if user is signed in then execute the following codes
+
+    /*    Intent i = new Intent(SecondActivity.this, HomeActivity.class);
+        startActivity(i); // invoke the SecondActivity.
+        finish();*/
+
+        //if user is not signed in the exucute the follow codes
+
+
+
+
     }
 }
