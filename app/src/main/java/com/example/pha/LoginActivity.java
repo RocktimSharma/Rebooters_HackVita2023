e17a95f4970cae2184b72df1a9bbd4b1a31de36f
Merlin <gauravnetuser@gmail.com>
2023-03-27 16:38:31 +0530

    new loginActivity

--- app/src/main/java/com/example/pha/LoginActivity.java
+++ app/src/main/java/com/example/pha/LoginActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
     private EditText email_edTxt,password_edTxt;
     private Button signIn_btn;
     private TextView forgetPassword_txt,register_txt;
@@ -22,21 +37,45 @@ public class LoginActivity extends AppCompatActivity {
         email_edTxt= findViewById(R.id.loginA_email_edtxt);
         password_edTxt=findViewById(R.id.loginA_password_edtxt);
         register_txt=findViewById(R.id.loginA_register_txtVw);
+
+        mAuth = FirebaseAuth.getInstance();
         register_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Intent is used to switch from one activity to another.
                 Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(i); // invoke the RegisterActivity.
-
+                finish();
             }
         });
+
 
         signIn_btn=findViewById(R.id.loginA_signin_btn);
         signIn_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = email_edTxt.getText().toString();
+                String pwd = password_edTxt.getText().toString();
                 //Write sign in codes here
+                if (TextUtils.isEmpty(email)) {
+                    email_edTxt.setError("Email cannot be empty");
+                    email_edTxt.requestFocus();
+                } else if (TextUtils.isEmpty(pwd)) {
+                    password_edTxt.setError("Password cannot be empty");
+                    password_edTxt.requestFocus();
+                } else {
+                    mAuth.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                       @Override
+                        public void onComplete(@NonNull Task<AuthResult> task){
+                           if (task.isSuccessful()){
+                               Toast.makeText(LoginActivity.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                               startActivity(new Intent(LoginActivity.this, UserProfile.class));
+                           } else {
+                               Toast.makeText(LoginActivity.this, "Oh no" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                           }
+                       }
+                    });
+                }
             }
         });
 
--- app/src/main/java/com/example/pha/RegisterActivity.java
+++ app/src/main/java/com/example/pha/RegisterActivity.java
@@ -1,17 +1,27 @@
 package com.example.pha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText email_edTxt,password_edTxt,name_edTxt,phone_edTxt;
-
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://personalheathassistant-8b043-default-rtdb.firebaseio.com");
     private Button register_btn;
     private TextView logIn_txt;
     @Override
@@ -27,7 +37,36 @@ public class RegisterActivity extends AppCompatActivity {
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String name = name_edTxt.getText().toString();
+                final String phone = phone_edTxt.getText().toString();
+                final String email = email_edTxt.getText().toString();
+                final String pwd = password_edTxt.getText().toString();
 
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(phone) || TextUtils.isEmpty(email) || TextUtils.isEmpty(pwd)) {
+                    Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                       public void onDataChange(@NonNull DataSnapshot snapshot) {
+                           //if not registered before
+                           if (snapshot.hasChild(phone)){
+                               Toast.makeText(RegisterActivity.this, "Phone is already registered", Toast.LENGTH_SHORT).show();
+                           }else{
+                               //string data to firebase
+                               databaseReference.child("users").child(phone).child("name").setValue(name);
+                               databaseReference.child("users").child(phone).child("email").setValue(email);
+                               databaseReference.child("users").child(phone).child("pwd").setValue(pwd);
+
+                               Toast.makeText(RegisterActivity.this, "User register successfully", Toast.LENGTH_SHORT).show();
+                               finish();
+                           }
+                       }
+                       @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                       }
+                    });
+
+                }
             }
         });
         logIn_txt.setOnClickListener(new View.OnClickListener() {
