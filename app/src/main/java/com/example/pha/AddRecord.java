4c91695fa2c13f218b5ff2ad178db357001662e3
rocktim <rocktimsharma82@gmail.com>
2023-03-30 17:17:55 +0530

    By Rocktim

--- app/src/main/java/com/example/pha/AddRecord.java
+++ app/src/main/java/com/example/pha/AddRecord.java
@@ -1,11 +1,31 @@
 package com.example.pha;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.io.IOException;
 
 public class AddRecord extends AppCompatActivity {
     private ImageView photo_imgVw;
@@ -29,5 +49,66 @@ public class AddRecord extends AppCompatActivity {
 
 
 
+
+
     }
+/*
+    private void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        launchSomeActivity.launch(i);
+    }
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap = null;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    getActivity().getContentResolver(),
+                                    selectedImageUri);
+                            dp_imVw.setImageBitmap(selectedImageBitmap);
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                            // Code for showing progressDialog while uploading
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(selectedImageUri)
+                                    .build();
+
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d(TAG, "User profile updated.");
+                                                Toast.makeText(getActivity(), "Profile successfully updated", Toast.LENGTH_LONG).show();
+
+                                            }
+                                        }
+                                    });
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+
+*/
+
+
 }
